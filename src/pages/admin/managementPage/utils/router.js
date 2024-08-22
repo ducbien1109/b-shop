cc35d795cc89ddb28ab385bb99699823a49d4682
ducbien1109 <ducbien911@gmail.com>
2024-08-22 12:20:35 +0700

    main

---
+++ src/pages/admin/managementPage/utils/router.js
@@ -0,0 +1,15 @@
+export const ROUTERS = {
+    ADMIN: {
+        ADD_PRODUCT: "add-product",
+        EDIT_PRODUCT: "edit-product",
+        LIST_OF_PRODUCTS_AND_CATEGORIES: "categories-and-products",
+        EDIT_BANNER: "edit-banner",
+        ADD_ACCOUNT: "add-account",
+        ACCOUNT_LIST: "account-list",
+        ADD_COMMENT: "add-comment",
+        COMMENT_LIST: "comment-list",
+        ORDER_LIST: "order-list",
+        STORE_INFORMATION: "store-information",
+        SALES_PRODUCT_QUANTITY_STATISTICS:"sales-product-quantity-statistics",
+    }
+}
